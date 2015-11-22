541a93fc34abde43aff6b781d0645bafb003caf8
Patric Vormstein <patric@vormstein.de>
2015-11-22 15:21:45 +0100

    initial commit

---
+++ examples/replay.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var Parser = require('../lib/rocket_league_parser.js');
+var Replay = require('../lib/rocket_league_replay.js').Replay;
+
+testfile = './test/replays/002E80F0452C36768F041E998A1C2C18.replay';
+//testfile2 = './test/4A5B13DA4D3C650D729A98B85D350FF7.replay';
+//testfileold = './test/626D0A274F5C721C033F75A4CD622C4B.replay';
+
+fs.readFile(testfile, function(err, buf) {
+	if(err) {
+		console.log(err.message);
+		return;
+	}
+
+	// Parse replay an rebuild it beautifully
+	var parsedReplay = Parser.parse(buf);
+	var replay = new Replay(parsedReplay);
+
+	// Debug it
+	console.log(require('util').inspect(replay, { depth: null }));
+});
