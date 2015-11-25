4c52117d5abc29a1c642656589f8089a6d1dadf8
Patric Vormstein <patric@vormstein.de>
2015-11-25 02:05:00 +0100

    Added debugging log to the parser

--- examples/debug_parsed_replay.js
+++ examples/debug_parsed_replay.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var RocketLeagueParser = require('../lib/rocket_league_replay.js').Parser;
 
 testfile = './test/replays/002E80F0452C36768F041E998A1C2C18.replay';
-//testfile2 = './test/4A5B13DA4D3C650D729A98B85D350FF7.replay';
-//testfileold1 = './test/626D0A274F5C721C033F75A4CD622C4B.replay';
+testfile2 = './test/replays/4A5B13DA4D3C650D729A98B85D350FF7.replay';
+testfileold = './test/replays/626D0A274F5C721C033F75A4CD622C4B.replay';
 
 fs.readFile(testfile, function(err, buf) {
 	if(err) {
--- examples/replay.js
+++ examples/replay.js
@@ -3,8 +3,8 @@ var RocketLeagueParser = require('../lib/rocket_league_replay.js').Parser;
 var Replay = require('../lib/rocket_league_replay.js').Replay;
 
 testfile = './test/replays/002E80F0452C36768F041E998A1C2C18.replay';
-//testfile2 = './test/4A5B13DA4D3C650D729A98B85D350FF7.replay';
-//testfileold = './test/626D0A274F5C721C033F75A4CD622C4B.replay';
+testfile2 = './test/replays/4A5B13DA4D3C650D729A98B85D350FF7.replay';
+testfileold = './test/replays/626D0A274F5C721C033F75A4CD622C4B.replay';
 
 fs.readFile(testfile, function(err, buf) {
 	if(err) {
